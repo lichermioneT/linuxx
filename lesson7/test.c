e800c166293cab940399dc3ae03194dcb65fd7ae
lic <2924942127@qq.com>
2025-09-17 21:38:31 +0800

    vim

---
+++ lesson7/test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("hello world\n");
+  printf("Txsdfsdfsdd\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
