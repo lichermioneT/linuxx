de94eee9564295e43cef80a8da437b38498c8ad4
lic <2924942127@qq.com>
2025-09-19 17:58:58 +0800

    gcc

---
+++ lesson8/process.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main()
+{
+  
+  // 毫无怀疑的是，一定执行的是printf
+  // 这是一个顺序结构的
+  //
+  // 只不过该数据没有被显示出来
+  // sleep2才显示出来的
+  // sleep2期间 printf已经完成打印了，那么对应的数据在哪里呢？
+  // 缓冲区里面
+  // 现在无法解释系统级别的缓冲区
+  // \n行缓冲区域的
+  // printf("you can see me..........\n");
+  
+  if(0)
+  {
+    
+  printf("you can see me..........");
+  fflush(stdout);
+  sleep(2);
+  printf("\n");
+  }
+  
+
+  // 
+  int cnt = 12;
+  while(cnt)
+  {
+    //printf("%d \r",cnt);
+    printf("剩余时间：%2d \r",cnt); // 一次刷新两个字符
+    fflush(stdout);
+    cnt--;
+    sleep(1);
+  }
+
+
+
+
+
+
+  return 0;
+}
+
+
