261e8b9436906e358476c756223b442a9055ac48
lic <2924942127@qq.com>
2025-09-21 11:43:26 +0800

    进程地址空间

---
+++ lesson11/process.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+
+  while(1)
+  {
+    printf("我是一个进程 我的id:%d \n", getpid());
+    sleep(1);
+  }
+
+  return 0;
+}
---
+++ lesson11/test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+
+
+  printf("hello OS\n");
+
+
+
+
+
+  return 0;
+}
