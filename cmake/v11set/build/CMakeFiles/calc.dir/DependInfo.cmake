2cc630ba3cc95ed7afb92f7ab0a31e7a465299a5
lic <2924942127@qq.com>
2025-12-03 21:56:18 +0800

    cmake

---
+++ cmake/v11set/build/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/li/Desktop/gitee/linuxx/cmake/v11set/src/add.cpp" "/home/li/Desktop/gitee/linuxx/cmake/v11set/build/CMakeFiles/calc.dir/src/add.cpp.o"
+  "/home/li/Desktop/gitee/linuxx/cmake/v11set/src/div.cpp" "/home/li/Desktop/gitee/linuxx/cmake/v11set/build/CMakeFiles/calc.dir/src/div.cpp.o"
+  "/home/li/Desktop/gitee/linuxx/cmake/v11set/src/mul.cpp" "/home/li/Desktop/gitee/linuxx/cmake/v11set/build/CMakeFiles/calc.dir/src/mul.cpp.o"
+  "/home/li/Desktop/gitee/linuxx/cmake/v11set/src/sub.cpp" "/home/li/Desktop/gitee/linuxx/cmake/v11set/build/CMakeFiles/calc.dir/src/sub.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "calc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
